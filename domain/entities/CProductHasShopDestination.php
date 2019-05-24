b8966494f83e4a250896189d93a6a1fcdacf8c86
Juri Fiorani <jurif@hotmail.com>
2019-05-24 23:14:22 +0200

    Assegnazione Prodotti su shop Paralleli

---
+++ domain/entities/CProductHasShopDestination.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\base\CObjectCollection;
+use bamboo\core\db\pandaorm\entities\AEntity;
+
+/**
+ * Class CProductHasShopDestination
+ * @package bamboo\domain\entities
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *A
+ * @date 24/05/2019
+ * @since 1.0
+ */
+class CProductHasShopDestination extends AEntity
+{
+    protected $entityTable = 'ProductHasShopDestination';
+    protected $primaryKeys = ['id'];
+}
