044595acee913e679e8e4b4a0f8b1ae9127357f2
juri fiorani <jurif@hotmail.com>
2019-10-24 18:27:37 +0200

    creazione Vista Vendite

---
+++ domain/entities/CProductHasProductPhoto.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\base\CObjectCollection;
+use bamboo\core\db\pandaorm\entities\AEntity;
+use bamboo\core\db\pandaorm\repositories\CRepo;
+use bamboo\core\exceptions\BambooException;
+use bamboo\core\exceptions\BambooOutOfBoundException;
+use bamboo\core\utils\slugify\CSlugify;
+use bamboo\domain\repositories\CDocumentRepo;
+use bamboo\domain\repositories\CProductHistoryRepo;
+
+/**
+ * Class CProduct
+ * @package bamboo\domain\entities
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 01/11/2014
+ * @since 0.0.1
+ *
+ * @property CProductBrand $productBrand
+ * @property CProductSeason $productSeason
+ * @property CProductVariant $productVariant
+ * @property CObjectCollection $productSheetActual
+ * @property CProductSheetPrototype $productSheetPrototype
+ * @property CObjectCollection $productSku
+ * @property CObjectCollection $productPublicSku
+ * @property CObjectCollection $productPhoto
+ * @property CProductColorGroup $productColorGroup
+ * @property CObjectCollection $productCategoryTranslation
+ * @property CObjectCollection $productCategory
+ * @property CObjectCollection $shop
+ * @property CObjectCollection $productNameTranslation
+ * @property CProductSizeGroup $productSizeGroup
+ * @property CObjectCollection $tag
+ * @property CProductStatus $productStatus
+ * @property CObjectCollection $shopHasProduct
+ * @property CObjectCollection $productDescriptionTranslation
+ * @property CObjectCollection $marketplaceAccountHasProduct
+ * @property CObjectCollection $cartLine
+ * @property CObjectCollection $orderLine
+ * @property CObjectCollection $shooting
+ * @property CProductCardPhoto $productCardPhoto
+ * @property CObjectCollection $productHasTag
+ * @property CObjectCollection $productEan
+ * @property CPrestashopHasProduct $prestashopHasProduct
+ * @property CProductBrandHasPrestashopManufacturer $productBrandHasPrestashopManufacturer
+ * @property CProductColorGroupHasPrestashopColorOption $productColorGroupHasPrestashopColorOption
+ *
+ *
+ */
+class CProductHasProductPhoto extends AEntity
+{
+    protected $entityTable = 'ProductHasProductPhoto';
+    protected $primaryKeys = ['productId', 'productVariantId','ProductPhotoId'];
+
+}
