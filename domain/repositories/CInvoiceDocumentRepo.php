45c8fb0f09137807ab198bad00d52eb135f7c6ad
juri fiorani <jurif@hotmail.com>
2019-11-13 19:30:26 +0100

    creazione Vista Vendite

--- domain/repositories/CInvoiceDocumentRepo.php
+++ domain/repositories/CInvoiceDocumentRepo.php
@@ -2,7 +2,10 @@
 namespace bamboo\domain\repositories;
 
 use bamboo\core\db\pandaorm\repositories\ARepo;
+use bamboo\core\exceptions\BambooException;
 use bamboo\domain\entities\CInvoiceDocument;
+use PDO;
+use PDOException;
 
 /**
  * Class CInvoiceDocumentRepo
@@ -30,6 +33,97 @@ class CInvoiceDocumentRepo extends ARepo
             $invDoc->fileName = $file["name"];
             $invDoc->type = $type;
             $invDoc->smartInsert();
+            $orderRepo=\Monkey::app()->repoFactory->create('Order')->findOneBy(['Order'=>$orderId]);
+            $remoteShopSellerId=$orderRepo->remoteShopSellerId;
+            $remoteOrderSellerId=$orderRepo->remoteOrderSellerId;
+            $findShopId = \Monkey::app()->repoFactory->create('Shop')->findOneBy(['id' => $remoteShopSellerId]);
+            if ($findShopId->hasEcommerce == '1' && $findShopId->id != '44') {
+                /* find  orderId*/
+
+                $db_host = $findShopId->dbHost;
+                $db_name = $findShopId->dbName;
+                $db_user = $findShopId->dbUsername;
+                $db_pass = $findShopId->dbPassword;
+                try {
+
+                    $db_con = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
+                    $db_con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                    $res = " connessione ok <br>";
+                } catch (PDOException $e) {
+                    throw new BambooException('fail to connect');
+
+                }
+                try {
+                    $stmtInsertSellerInvoiceDocument = $db_con->prepare('INSERT INTO Invoice (orderId, bin, date,fileName,type) 
+                                                                                    VALUES(
+                                                                                         \'' . $remoteOrderSellerId . '\',
+                                                                                         \'' . file_get_contents($file["tmp_name"]) . '\',
+                                                                                         \'date_format(NOW(),\'%Y-%m-%d %H:%i:%s\')\',
+                                                                                         \'' . $file["name"] . '\',
+                                                                                         \'' . $type . '\'
+                                                                                    ) ');
+                    $stmtInsertSellerInvoiceDocument->execute();
+                } catch (PDOException $e) {
+                    $sql = 'INSERT INTO Invoice (orderId, bin, date,fileName,type) 
+                                                                                    VALUES(
+                                                                                         \'' . $remoteOrderSellerId . '\',
+                                                                                         \'' . file_get_contents($file["tmp_name"]) . '\',
+                                                                                         \'date_format(NOW(),\'%Y-%m-%d %H:%i:%s\')\',
+                                                                                         \'' . $file["name"] . '\',
+                                                                                         \'' . $type . '\'
+                                                                                    ) ';
+                    \Monkey::app()->applicationLog('CInvoiceDocumentRepo','error','invoicedocumentSellerRemote',$sql,'');
+                }
+            }
+                $orderLines=\Monkey::app()->repoFactory->create('OrderLine')->findBy(['orderId'=>$orderId]);
+                foreach($orderLines as $orderLine) {
+                    if ($orderLine->remoteOrderSupplierId != null) {
+                        $findShopSupplier = \Monkey::app()->repoFactory->create('Shop')->findOneBy(['id' => $orderLine->shopId]);
+                        if ($findShopSupplier->hasEcommerce == '1' && $findShopSupplier->id != '44') {
+                            /* find  orderId*/
+
+                            $db_host1 = $findShopSupplier->dbHost;
+                            $db_name1 = $findShopSupplier->dbName;
+                            $db_user1 = $findShopSupplier->dbUsername;
+                            $db_pass1 = $findShopSupplier->dbPassword;
+                            try {
+
+                                $db_con1 = new PDO("mysql:host={$db_host1};dbname={$db_name1}",$db_user1,$db_pass1);
+                                $db_con1->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                                $res = " connessione ok <br>";
+                            } catch (PDOException $e) {
+                                throw new BambooException('fail to connect');
+
+                            }
+
+                            try {
+                                $stmtInsertSupplierInvoiceDocument = $db_con->prepare('INSERT INTO Invoice (orderId, bin, date,fileName,type) 
+                                                                                    VALUES(
+                                                                                         \'' . $orderLine->remoteOrderSupplierId . '\',
+                                                                                         \'' . file_get_contents($file["tmp_name"]) . '\',
+                                                                                         \'date_format(NOW(),\'%Y-%m-%d %H:%i:%s\')\',
+                                                                                         \'' . $file["name"] . '\',
+                                                                                         \'' . $type . '\'
+                                                                                    ) ');
+                                $stmtInsertSupplierInvoiceDocument->execute();
+                            } catch (PDOException $e) {
+                                $sql = 'INSERT INTO Invoice (orderId, bin, date,fileName,type) 
+                                                                                    VALUES(
+                                                                                         \'' . $orderLine->remoteOrderSupplierId . '\',
+                                                                                         \'' . file_get_contents($file["tmp_name"]) . '\',
+                                                                                         \'date_format(NOW(),\'%Y-%m-%d %H:%i:%s\')\',
+                                                                                         \'' . $file["name"] . '\',
+                                                                                         \'' . $type . '\'
+                                                                                    ) ';
+                                \Monkey::app()->applicationLog('CInvoiceDocumentRepo','error','invoicedocumentSupplierRemote',$sql,'');
+                            }
+                        }
+                    }
+
+
+                }
+
+
         } catch (\Throwable $e){
             return false;
         }
