8a087b151afbca0a81f318dee19806fa92c6daaa
juri fiorani <jurif@hotmail.com>
2020-01-17 19:18:45 +0100

    creazione Vista Vendite

---
+++ domain/entities/CAggregatorPublishLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\db\pandaorm\entities\AEntity;
+
+/**
+ * Class CAggregatorPublishLog
+ * @package bamboo\domain\entities
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 17/01/2020
+ * @since 1.0
+ */
+class CAggregatorPublishLog extends AEntity
+{
+    protected $entityTable = 'CAggregatorPublishLog';
+    protected $primaryKeys = ['id'];
+}
