80115dc85de3ac11332eff68988e9ffea033796d
juri fiorani <jurif@hotmail.com>
2020-02-10 18:04:46 +0100

    creazione Vista Vendite

---
+++ domain/entities/CBillRegistryProductDetail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\db\pandaorm\entities\AEntity;
+
+/**
+ * Class CBillRegistryProductDetail
+ * @package bamboo\domain\entities
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 27/01/2020
+ * @since 1.0
+ */
+
+class CBillRegistryProductDetail extends AEntity
+{
+
+    protected $entityTable = 'BillRegistryProductDetail';
+    protected $primaryKeys = ['id'];
+}
