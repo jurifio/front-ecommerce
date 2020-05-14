bfa4c95913741283c6a01e07022a6696e5a3fadc
juri fiorani <jurif@hotmail.com>
2020-05-14 07:47:09 +0200

    creazione Vista Vendite

--- offline/feed/CGoogleFeedExpertBuilder.php
+++ offline/feed/CGoogleFeedExpertBuilder.php
@@ -36,18 +36,22 @@ class CGoogleFeedExpertBuilder extends AExpertFeedBuilder
 
         $url = $this->app->baseUrl(false, 'https') . $this->marketplaceAccount->config['feedUrl'];
 
+
         $writer->startElement('rss');
         $writer->writeAttribute('xmlns:g', 'http://base.google.com/ns/1.0');
         $writer->writeAttribute('version', '2.0');
 
         $writer->startElement("channel");
         $writer->writeElement('title', 'Google Product Feed');
-        $writer->writeElement('link', $this->app->baseUrl(false, 'https'));
+        $shopFind=\Monkey::app()->repoFactory->create('Shop')->findOneBy(['id'=>$this->marketplaceAccount->config['shopId']]);
+        $shopUrl=$shopFind->urlSite;
+        $writer->writeElement('link', $shopUrl);
+        //$writer->writeElement('link', $this->app->baseUrl(false, 'https'));
 
         $writer->startElement('author');
         $writer->writeElement('name', 'iwes');
         $writer->endElement();
-        $writer->writeElement('description', $url);
+        $writer->writeElement('description', $shopUrl);
         $writer->writeElement('updated', date(DATE_ATOM, time()));
 
         /** INIZIO INSERIMENTO PRODOTTI */
@@ -72,7 +76,7 @@ class CGoogleFeedExpertBuilder extends AExpertFeedBuilder
         $writer = new \XMLWriter();
         $writer->openMemory();
         $writer->setIndent(!$this->minized);
-        if ($product->qty>0) {
+        if ($product->qty > 0 ) {
             $writer->startElement("item");
             $writer->writeElement('g:id',$product->printId());
 
@@ -116,8 +120,11 @@ class CGoogleFeedExpertBuilder extends AExpertFeedBuilder
             $categories = $product->getMarketplaceAccountCategoryIds($this->marketplaceAccount);
             $writer->writeElement('g:google_product_category',$categories[0]);
 
-            $baseUrlLang = $this->app->cfg()->fetch("paths","domain") . "/" . $this->lang->getLang();
-            $writer->writeElement('g:link','https://' . $product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
+           // $baseUrlLang = $this->app->cfg()->fetch("paths","domain") . "/" . $this->lang->getLang();
+            $shopFind=\Monkey::app()->repoFactory->create('Shop')->findOneBy(['id'=>$this->marketplaceAccount->config['shopId']]);
+            $shopUrl=$shopFind->urlSite;
+            $baseUrlLang=$shopUrl. "/" . $this->lang->getLang();
+            $writer->writeElement('g:link', $product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
             $writer->writeElement('g:mobile_link',$product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
             $writer->writeElement('g:image_link',$this->helper->image($product->getPhoto(1,843),'amazon'));
             for ($i = 2; $i < 8; $i++) {
@@ -130,16 +137,16 @@ class CGoogleFeedExpertBuilder extends AExpertFeedBuilder
 
 
             $writer->writeElement('g:availability',$avai > 0 ? 'in stock' : 'out of stock');
-            $writer->writeElement('sizes',implode(';',$sizes));
-            $writer->writeElement('g:size',implode(';',$sizes));
+            //$writer->writeElement('sizes',implode(';',$sizes));
+          //  $writer->writeElement('g:size',implode(';',$sizes));
             $priceActive = \Monkey::app()->repoFactory->create('ProductSku')->findOneBy(['productId' => $product->id,'productVariantId' => $product->productVariantId]);
 
             $price = number_format($priceActive->price,2,'.','');
 
-            $salePrice = number_format($priceActive->salePrice,2,'.','');
 
             $writer->writeElement('g:price',$price . ' EUR');
             if ($product->isOnSale == 1) {
+                $salePrice = number_format($priceActive->salePrice,2,'.','');
                 $writer->writeElement('g:sale_price',$salePrice . ' EUR');
             }
             $writer->writeElement('g:mpn',$product->itemno . ' ' . $product->productVariant->name);
@@ -147,14 +154,25 @@ class CGoogleFeedExpertBuilder extends AExpertFeedBuilder
             if (!is_null($product->productColorGroup)) {
                 $writer->writeElement('g:color',$product->productColorGroup->productColorGroupTranslation->getFirst()->name);
             }
+            $productEan =  \Monkey::app()->repoFactory->create('ProductEan')->findOneBy(['productId' => $product->id, 'productVariantId' => $product->productVariantId,'used'=>'1']);
+            if ($productEan != null) {
+                $ean = $productEan->ean;
+                $writer->writeElement('g:gtin',$ean);
+            }
             $writer->startElement('g:shipping');
-            $writer->writeElement('g:service','Courier');
+            $writer->writeElement('g:service','IT_StandardInternational');
             $writer->writeElement('g:price','10.00 EUR');
             $writer->endElement();
             $writer->startElement('g:shipping');
             $writer->writeElement('g:country','IT');
             $writer->writeElement('g:service','Courier');
             $writer->writeElement('g:price','5.00 EUR');
+            $writer->endElement();
+            $writer->startElement('g:shipping');
+            $writer->writeElement('g:service','IT_ExpeditedInternational');
+            $writer->writeElement('g:price','40.00 EUR');
+
+
             $writer->endElement();
             $writer->endElement();
         }
