4bdd04235ed3995d883349a258fcf187099a4ff9
juri fiorani <jurif@hotmail.com>
2020-07-22 10:49:26 +0200

    creazione Vista Vendite

--- offline/productsync/import/standard/ABluesealProductImporter.php
+++ offline/productsync/import/standard/ABluesealProductImporter.php
@@ -623,9 +623,9 @@ abstract class ABluesealProductImporter extends ACronJob implements IBluesealPro
                 /** Inserisco la variante*/
                 $variant = $variantFactory->getEmptyEntity();
                 $variant->name = $dirtyProduct->var;
-                if( $dirtyProduct->extend->colorDescription!=null) {
+              /*  if( $dirtyProduct->extend->colorDescription!=null) {
                     $variant->description = $dirtyProduct->extend->colorDescription;
-                }
+                }*/
 
 
                 /** Inserisco il prodotto */
