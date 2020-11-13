a4dbd0266de0ad63403d9be248c4f5f02da6f17a
juri fiorani <jurif@hotmail.com>
2020-11-13 09:41:32 +0100

    creazione Vista Vendite

---
+++ domain/entities/CProductHasProductLook.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\db\pandaorm\entities\AEntity;
+
+
+/**
+ * Class CProductLook
+ * @package bamboo\domain\entities
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 11/10/2020
+ * @since 1.0
+ */
+class CProductHasProductLook extends AEntity
+{
+    protected $entityTable = 'ProductHasProductLook';
+	protected $primaryKeys = ['id'];
+} 
