8969c4df447252a5a13bf711e7615c0b7cbfef0e
juri fiorani <jurif@hotmail.com>
2021-01-04 10:09:04 +0100

    creazione Vista Vendite

---
+++ domain/entities/CMarketplaceConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\db\pandaorm\entities\AEntity;
+
+/**
+ * Class CMarketplace
+ * @package bamboo\domain\entities
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 01/11/2014
+ * @since 0.0.1
+ */
+class CMarketplaceConfig extends AEntity
+{
+    protected $entityTable = 'MarketplaceConfig';
+    protected $primaryKeys = ['confKey','marketplaceId','marketplaceAccountId'];
+
+}
