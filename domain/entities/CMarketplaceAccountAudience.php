4722b7281948e5dc3e5cae1ac57931903f0d92c8
juri fiorani <jurif@hotmail.com>
2021-03-30 14:46:42 +0200

    creazione Vista Vendite

---
+++ domain/entities/CMarketplaceAccountAudience.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\application\AApplication;
+use bamboo\core\base\CObjectCollection;
+use bamboo\core\db\pandaorm\entities\AEntity;
+use bamboo\core\utils\amazonPhotoManager\S3Manager;
+use bamboo\core\utils\slugify\CSlugify;
+
+/**
+ * Class CMarketplaceAccountAudience
+ * @package bamboo\domain\entities
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 29/03/2021
+ * @since 1.0
+ */
+class CMarketplaceAccountAudience extends AEntity
+{
+    protected $entityTable = 'MarketplaceAccountAudience';
+    protected $primaryKeys = ['marketplaceId','marketplaceAccountId','marketplaceGenderCode'];
+}
---
+++ domain/entities/CProductSizeHasMarketplaceAccountSize.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\application\AApplication;
+use bamboo\core\base\CObjectCollection;
+use bamboo\core\db\pandaorm\entities\AEntity;
+use bamboo\core\utils\amazonPhotoManager\S3Manager;
+use bamboo\core\utils\slugify\CSlugify;
+
+/**
+ * Class CProductColorGroupHasMarketplaceColorGroup
+ * @package bamboo\domain\entities
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 30/03/2021
+ * @since 1.0
+ */
+class CProductSizeHasMarketplaceAccountSize extends AEntity
+{
+    protected $entityTable = 'ProductSizeHasMarketplaceAccountSize';
+    protected $primaryKeys = ['marketplaceId','marketplaceAccountId','marketplaceSizeId','productType','productSizeId'];
+}
