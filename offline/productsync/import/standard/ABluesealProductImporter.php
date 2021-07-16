b6a70b769b146322f90a991aea696b699890a5b5
juri fiorani <jurif@hotmail.com>
2021-07-16 20:15:40 +0200

    creazione Vista Vendite

--- offline/productsync/import/standard/ABluesealProductImporter.php
+++ offline/productsync/import/standard/ABluesealProductImporter.php
@@ -346,7 +346,7 @@ abstract class ABluesealProductImporter extends ACronJob implements IBluesealPro
             }else{
             $limitEnd=$limitEnd+500;
             $urlDef=$url.'&offset='.$limitStart.'&limit=500';
-                $this->report("fetchWebMultiplesFiles","url: " . $urlDef,null);
+
                 $ch = curl_init();
                 //$urlget = sprintf("%s:%s", $urlDef, http_build_query(false));
                 curl_setopt($ch,CURLOPT_URL,$urlDef);
@@ -362,7 +362,7 @@ abstract class ABluesealProductImporter extends ACronJob implements IBluesealPro
 
 
                 $filename = $localDir . '/import/' . time() . ($this->config->fetch('filesConfig','extension') ?? '.xml');
-                $this->report("fetchWebMultiplesFiles","filename: " . $filename,null);
+
                 if (empty($retValue)) {
                     $this->warning('fetchWebMultiplesFiles','Got Empty File!');
                 } else {
